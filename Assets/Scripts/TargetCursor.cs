00e45f73f7b8ef55aa50dd716f4facd1f570f200
RGV <ricardo.guzman.velasco@outlook.com>
2025-05-24 15:48:41 +0200

    bug: only waits for first click to move

--- Assets/Scripts/TargetCursor.cs
+++ Assets/Scripts/TargetCursor.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 
 internal class TargetCursor : MonoBehaviour
 {
-    public Task SelectTarget()
+    public async Task SelectTarget()
     {
+        while (!Input.GetMouseButtonDown(0))
+        {
+            await Task.Yield();
+        }
+        
         FindAnyObjectByType<Character>().GetComponent<Movimiento>().Hacia(transform.position + Vector3.right * 5f);
-        return Task.CompletedTask;
     }
 }
