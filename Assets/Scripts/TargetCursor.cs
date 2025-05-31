5b2dc0d73c5469484e722b7992c0a69a39e6fd4b
RGV <ricardo.guzman.velasco@outlook.com>
2025-05-31 11:02:39 +0200

    fix: flush input between character choices

--- Assets/Scripts/TargetCursor.cs
+++ Assets/Scripts/TargetCursor.cs
@@ -13,8 +13,15 @@ internal class TargetCursor : MonoBehaviour
             await Task.Yield();
         }
         HideCursor();
-
+        
+        FlushInput();
         return WhereThePlayerIsPointingTo();
+    }
+
+    static void FlushInput()
+    {
+        Input.ResetInputAxes();
+        Input.ResetPenEvents();
     }
 
     static bool ChoseLocation() => Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject();
