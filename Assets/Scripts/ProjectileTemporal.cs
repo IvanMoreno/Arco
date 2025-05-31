c97afb83e149ce02ecf90a3d382f6963f91a903b
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-05-31 10:16:31 +0200

    feat: introduce temporal projectile to avoid merge conflict

---
+++ Assets/Scripts/ProjectileTemporal.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+internal class ProjectileTemporal : MonoBehaviour
+{
+    [SerializeField] float distancePerTurn = 2;
+    [SerializeField] float speed = 10;
+    
+    public Task HacerLoQueTengaPendiente()
+    {
+        return Moverse();
+    }
+
+    async Task Moverse()
+    {
+        var targetPosition = TargetPosition();
+        while (!CumpleCondicionDelTurno(targetPosition))
+        {
+            transform.Translate(transform.up * speed * Time.deltaTime, Space.World);
+            await Task.Yield();
+        }
+    }
+
+    bool CumpleCondicionDelTurno(Vector3 targetPosition) => Vector3.Distance(transform.position, targetPosition) <= 0.1f;
+    Vector3 TargetPosition() => transform.position + transform.up * distancePerTurn;
+
+    public void Towards(Vector3 target)
+    {
+        transform.up = (target - transform.position).normalized;
+    }
+}
