46f011a8e15dfe77185c465c5aa0387d5daaaf19
RGV <ricardo.guzman.velasco@outlook.com>
2025-05-31 09:15:14 +0200

    feat: show and hide cursor

--- Assets/Scripts/TargetCursor.cs
+++ Assets/Scripts/TargetCursor.cs
@@ -5,15 +5,27 @@ internal class TargetCursor : MonoBehaviour
 {
     public async Task SelectTarget()
     {
+        ShowCursor();
         while (!Input.GetMouseButtonDown(0))
         {
             transform.position = WhereThePlayerIsPointingTo();
             await Task.Yield();
         }
+        HideCursor();
 
         FindAnyObjectByType<Character>()
             .GetComponent<Movimiento>()
             .Towards(WhereThePlayerIsPointingTo());
+    }
+
+    void ShowCursor()
+    {
+        GetComponentInChildren<SpriteRenderer>().enabled = true;
+    }
+    
+    void HideCursor()
+    {
+        GetComponentInChildren<SpriteRenderer>().enabled = false;
     }
 
     static Vector3 WhereThePlayerIsPointingTo()
