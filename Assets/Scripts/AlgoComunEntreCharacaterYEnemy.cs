9e76dd573add82da745ecdf22089ede2ecbf1f0a
RGV <ricardo.guzman.velasco@outlook.com>
2025-05-31 12:14:42 +0200

    refactor: extract an idea of what is common

---
+++ Assets/Scripts/AlgoComunEntreCharacaterYEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+internal class AlgoComunEntreCharacaterYEnemy : MonoBehaviour
+{
+    bool willAttackInThisTurn;
+
+    public Task HacerLoQueTengaPendiente()
+    {
+        return willAttackInThisTurn
+            ? GetComponent<Disparo>().Hacerse()
+            : GetComponent<Movimiento>().Hacerse();
+    }
+
+    public void ProgramarMovimiento(Vector3 target)
+    {
+        willAttackInThisTurn = false;
+        GetComponent<Movimiento>().Towards(target);
+    }
+    
+    public void ProgramarDisparo(Vector3 target)
+    {
+        willAttackInThisTurn = true;
+        GetComponent<Disparo>().Towards(target);
+    }
+
+    public void RecibirImpacto()
+    {
+        GetComponent<Vida>().UnaMenos();
+        if (GetComponent<Vida>().EstaMuerto())
+            Destroy(gameObject);
+    }
+}
