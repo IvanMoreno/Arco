229c86432bdaefc126a48ac8ab99bd97681ec555
RGV <ricardo.guzman.velasco@outlook.com>
2025-05-31 08:46:04 +0200

    feat: projectiles go faster

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,21 +3,19 @@ using UnityEngine;
 
 internal class Projectile : MonoBehaviour
 {
-    Vector3 target;
-    
     public Task HacerLoQueTengaPendiente()
     {
         return Moverse();
     }
 
-    Task Moverse()
+    async Task Moverse()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target, 5f * Time.deltaTime);
-        return Task.CompletedTask;
+        transform.Translate(transform.up * 2, Space.World);
+        
     }
 
     public void Towards(Vector3 target)
     {
-        this.target = target;
+        transform.up = (target - transform.position).normalized;
     }
 }
