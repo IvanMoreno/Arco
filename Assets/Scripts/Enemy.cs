440b379086c2bd05c6ff7d79b9843c02f5d8eda8
RGV <ricardo.guzman.velasco@outlook.com>
2025-05-31 07:58:03 +0200

    feat: whether enemy shoots or moves

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,7 +3,22 @@ using UnityEngine;
 
 internal class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject projectilePrefab;
+    
     public Task HacerLoQueTengaPendiente()
+    {
+        return Random.value < 0.5f ? Moverse() : DispararHaciaElPersonaje();
+    }
+
+    async Task DispararHaciaElPersonaje()
+    {
+        var posPersonaje = FindAnyObjectByType<Character>().transform.position;
+
+        var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        projectile.transform.up = (posPersonaje - transform.position).normalized;
+    }
+
+    Task Moverse()
     {
         GetComponent<Movimiento>().Hacia((Vector2)transform.position + Random.insideUnitCircle);
         return GetComponent<Movimiento>().Hacerse();
