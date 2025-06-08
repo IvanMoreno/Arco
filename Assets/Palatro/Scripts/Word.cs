9729d098a6b2ef3af6bfdb9e8b374d7bb4bf5c7e
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-06-08 09:46:16 +0200

    feat: calculate word points

--- Assets/Palatro/Scripts/Word.cs
+++ Assets/Palatro/Scripts/Word.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using NUnit.Framework;
 
 namespace Palatro
 {
@@ -7,9 +8,35 @@ namespace Palatro
     {
         readonly IEnumerable<Letter> letters;
         public string Shape => letters.Aggregate(string.Empty, (word, letter) => word + letter.Shape);
+        public int Points
+        {
+            get
+            {
+                var result = 0;
+                for (var i = 0; i < letters.Count(); i++)
+                {
+                    result += letters.ElementAt(i).Points + ExtraPointsFromPosition(i);
+                }
+                return result;
+            }
+        }
 
         Word(IEnumerable<Letter> letters) => this.letters = letters;
 
         public static Word From(IEnumerable<Letter> letters) => new(letters);
+        
+        // Esto pertenece aquí?
+        public static int ExtraPointsFromPosition(int i)
+        {
+            Assert.IsTrue(i is >= 0 and < 10, $"Index {i} is out of range. It should be between 1 and 9.");
+            return i switch
+            {
+                < 4 => 0,
+                5 => 2,
+                6 or 7 or 8 => 5,
+                9 => 10,
+                _ => 0
+            };
+        }
     }
 }
