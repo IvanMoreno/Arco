bd7ac4a77aba3fa1737d2b91f63756453ba1fde4
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-06-08 08:20:51 +0200

    feat: scratch attempt panel api

---
+++ Assets/Palatro/Scripts/AttemptPanel.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Palatro
+{
+    public class AttemptPanel : MonoBehaviour
+    {
+        public bool IsEmpty { get; }
+
+        public void Play(LetterToPlay letter)
+        {
+            
+        }
+
+        public void RemoveLastLetter()
+        {
+            
+        }
+
+        public LetterToPlay GetLastLetter()
+        {
+            return null;
+        }
+    }
+}
