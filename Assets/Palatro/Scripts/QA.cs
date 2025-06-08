189aa11182c44d31fc1ef47630d00c7a64160870
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-06-08 10:40:08 +0200

    qa: hardcode reroll

---
+++ Assets/Palatro/Scripts/QA.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using UnityEngine;
+
+namespace Palatro
+{
+    public class QA : MonoBehaviour
+    {
+        const string HardcodedValidWords = "HABITATION";
+        
+        [ContextMenu("Hardcode Reroll")]
+        public void HardcodeReroll()
+        {
+            foreach (var tile in FindObjectsByType<TileToPlay>(FindObjectsSortMode.None))
+            {
+                tile.Resemble(new(".", 1));
+            }
+            
+            for (var i = 0; i < HardcodedValidWords.Length; i++)
+            {
+                var letter = new Letter(HardcodedValidWords[i].ToString(), 1);
+                FindObjectsByType<TileToPlay>(FindObjectsSortMode.None)
+                    .OrderBy(tile => tile.transform.GetSiblingIndex())
+                    .ElementAt(i)
+                    .Resemble(letter);
+            }
+        }
+    }
+}
