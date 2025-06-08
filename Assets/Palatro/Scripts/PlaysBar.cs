c3ede47c443f7610fd36f7dfdd1f09efd7efa2fa
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-08 11:34:32 +0200

    refactor: sketch plays api

--- Assets/Palatro/Scripts/PlaysBar.cs
+++ Assets/Palatro/Scripts/PlaysBar.cs
@@ -1,9 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace Palatro
 {
     public class PlaysBar : MonoBehaviour
     {
+        int attemptsLeft;
+        int maxAttempts;
         
+        float ProportionOfAttemptsLeft => (float)attemptsLeft / maxAttempts;
+        
+        public void BeginWith(int amountOfAttempts)
+        {
+            Assert.IsTrue(amountOfAttempts > 0);
+            
+            maxAttempts = amountOfAttempts;
+            attemptsLeft = amountOfAttempts;
+        }
+        public Task DownByOne()
+        {
+            Assert.IsTrue(attemptsLeft > 0);
+            
+            attemptsLeft--;
+            return UpdateBar();
+        }
+
+        Task UpdateBar()
+        {
+            var fill = transform.Find("Fill").GetComponent<Image>();
+            fill.fillAmount = ProportionOfAttemptsLeft;
+            
+            var text = transform.Find("RemainingPlays").GetComponent<Text>();
+            text.text = attemptsLeft.ToString();
+            
+            return Task.CompletedTask;
+        }
     }
 }
