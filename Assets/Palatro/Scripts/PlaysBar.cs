561413e4abccf21b04b3c4cb9c878246b71cca48
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-08 12:25:02 +0200

    feat: plays bar badge

--- Assets/Palatro/Scripts/PlaysBar.cs
+++ Assets/Palatro/Scripts/PlaysBar.cs
@@ -40,14 +40,37 @@ namespace Palatro
             return UpdateBar();
         }
 
-        Task UpdateBar()
+        async Task UpdateBar()
         {
+            await FirstShowBadge();
+            
             var text = transform.Find("RemainingPlays").GetComponent<TMP_Text>();
             text.text = attemptsLeft.ToString();
             
-            return transform.Find("Fill").GetComponent<Image>().DOFillAmount(ProportionOfAttemptsLeft, 0.5f)
+            await transform.Find("Fill").GetComponent<Image>().DOFillAmount(ProportionOfAttemptsLeft, 0.5f)
                 .SetEase(Ease.OutCubic)
                 .AsyncWaitForCompletion();
+
+            await FadeOutBadge();
+        }
+
+        async Task FadeOutBadge()
+        {
+            var badge = transform.Find("BadgeClone");
+            await badge.GetComponent<CanvasGroup>().DOFade(0, 0.5f)
+                .SetEase(Ease.InCubic)
+                .AsyncWaitForCompletion();
+            Destroy(badge.gameObject);
+        }
+
+        Task FirstShowBadge()
+        {
+            var originalBadge = transform.Find("Badge");
+            var clone = Instantiate(originalBadge, originalBadge.position, originalBadge.rotation, transform);
+            clone.name = "BadgeClone";
+            clone.gameObject.SetActive(true);
+            
+            return clone.DOLocalMoveY(-100, 0.5f).From().SetEase(Ease.OutBack).AsyncWaitForCompletion();
         }
     }
 }
