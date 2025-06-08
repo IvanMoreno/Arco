a0c2ef30a03a3efa65dfd2b3ae42627a174aa605
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-08 08:12:36 +0200

    build: reference text mesh pro, sketch extra points of attempt

--- Assets/Palatro/Scripts/LetterOfAttempt.cs
+++ Assets/Palatro/Scripts/LetterOfAttempt.cs
@@ -1,11 +1,27 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class LetterOfAttempt : MonoBehaviour
 {
+    public int ExtraPoints { get; private set; }
+    
     void Awake()
     {
         ToggleIsEmpty(true);
+        ExtraPointsAre(ExtraPointsFromPosition(transform.GetSiblingIndex()));
+    }
+
+    public void ExtraPointsAre(int howMany)
+    {
+        Assert.IsTrue(howMany >= 0);
+
+        return; //not working due to null.
+        ExtraPoints = howMany;
+        var extraPointsText = transform.Find("ExtraPointsText");
+        
+        extraPointsText.GetComponentInChildren<TMP_Text>().text = howMany > 0 ? "+" + howMany : "";
     }
 
     void ToggleIsEmpty(bool isEmpty)
@@ -15,5 +31,18 @@ public class LetterOfAttempt : MonoBehaviour
         
         filledState.gameObject.SetActive(!isEmpty);
         emptyState.gameObject.SetActive(isEmpty);
+    }
+
+    static int ExtraPointsFromPosition(int i)
+    {
+        Assert.IsTrue(i is >= 0 and < 10, $"Index {i} is out of range. It should be between 1 and 9.");
+        return i switch
+        {
+            < 4 => 0,
+            5 => 2,
+            6 or 7 or 8 => 5,
+            9 => 10,
+            _ => 0
+        };
     }
 }
