47fae09d41b25665081d6d9b848b9535e2bfef76
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-08 09:36:26 +0200

    refactor: attach bag to scene

--- Assets/Palatro/Scripts/Bag.cs
+++ Assets/Palatro/Scripts/Bag.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Palatro
 {
     public class Bag : MonoBehaviour
     {
-        
+        public IEnumerable<Letter> Pick(int count)
+        {
+            var letters = new List<Letter>();
+            for (var i = 0; i < count; i++)
+                letters.Add(Alphabet.Random());
+            return letters;
+        }
     }
 }
