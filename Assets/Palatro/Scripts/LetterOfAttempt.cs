42cd47bb08581dc19aed3310f248d36c3aca027f
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-08 08:26:31 +0200

    refactor: sketch what will happen when placing a letter in attempt

--- Assets/Palatro/Scripts/LetterOfAttempt.cs
+++ Assets/Palatro/Scripts/LetterOfAttempt.cs
@@ -1,47 +1,62 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class LetterOfAttempt : MonoBehaviour
+namespace Palatro
 {
-    public int ExtraPoints { get; private set; }
-    
-    void Awake()
+    public class LetterOfAttempt : MonoBehaviour
     {
-        ToggleIsEmpty(true);
-        ExtraPointsAre(ExtraPointsFromPosition(transform.GetSiblingIndex()));
-    }
-
-    void ExtraPointsAre(int howMany)
-    {
-        Assert.IsTrue(howMany >= 0);
-
-        ExtraPoints = howMany;
-        var extraPointsText = transform.Find("WhenIsEmpty/ExtraPoints");
+        public int ExtraPoints { get; private set; }
+        LetterToPlay filledWith;
         
-        extraPointsText.GetComponentInChildren<TMP_Text>().text = howMany > 0 ? "+" + howMany : "";
-    }
-
-    void ToggleIsEmpty(bool isEmpty)
-    {
-        var filledState = transform.Find("WhenIsFilledWithLetter");
-        var emptyState = transform.Find("WhenIsEmpty");
-        
-        filledState.gameObject.SetActive(!isEmpty);
-        emptyState.gameObject.SetActive(isEmpty);
-    }
-
-    static int ExtraPointsFromPosition(int i)
-    {
-        Assert.IsTrue(i is >= 0 and < 10, $"Index {i} is out of range. It should be between 1 and 9.");
-        return i switch
+        void Awake()
         {
-            < 4 => 0,
-            5 => 2,
-            6 or 7 or 8 => 5,
-            9 => 10,
-            _ => 0
-        };
+            ToggleIsEmpty(true);
+            ExtraPointsAre(ExtraPointsFromPosition(transform.GetSiblingIndex()));
+        }
+
+        public void Put(LetterToPlay letter)
+        {
+            Assert.IsNotNull(letter);
+            Assert.IsNull(filledWith);
+            
+            filledWith = letter;
+            ToggleIsEmpty(false);
+        }
+
+        void ExtraPointsAre(int howMany)
+        {
+            Assert.IsTrue(howMany >= 0);
+
+            ExtraPoints = howMany;
+            var extraPointsText = transform.Find("WhenIsEmpty/ExtraPoints");
+        
+            extraPointsText.GetComponentInChildren<TMP_Text>().text = howMany > 0 ? "+" + howMany : "";
+        }
+
+        void ToggleIsEmpty(bool isEmpty)
+        {
+            transform.Find("WhenIsEmpty").gameObject.SetActive(isEmpty);
+            transform.Find("WhenIsFilledWithLetter").gameObject.SetActive(!isEmpty);
+
+            if (!isEmpty)
+            {
+                transform.Find("WhenIsFilledWithLetter/Character").GetComponent<TMP_Text>().text = filledWith.Letter;
+                transform.Find("WhenIsFilledWithLetter/Points").GetComponent<TMP_Text>().text = filledWith.Points.ToString();
+            }
+        }
+
+        static int ExtraPointsFromPosition(int i)
+        {
+            Assert.IsTrue(i is >= 0 and < 10, $"Index {i} is out of range. It should be between 1 and 9.");
+            return i switch
+            {
+                < 4 => 0,
+                5 => 2,
+                6 or 7 or 8 => 5,
+                9 => 10,
+                _ => 0
+            };
+        }
     }
 }
