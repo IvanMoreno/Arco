363fd928bafc8ff5532da00968b228e106ee9f12
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-08 08:03:19 +0200

    feat: hide filled letters of attempts on play

--- Assets/Palatro/Scripts/LetterOfAttempt.cs
+++ Assets/Palatro/Scripts/LetterOfAttempt.cs
@@ -1,6 +1,19 @@
+using System;
 using UnityEngine;
 
 public class LetterOfAttempt : MonoBehaviour
 {
-    
+    void Awake()
+    {
+        ToggleIsEmpty(true);
+    }
+
+    void ToggleIsEmpty(bool isEmpty)
+    {
+        var filledState = transform.Find("WhenIsFilledWithLetter");
+        var emptyState = transform.Find("WhenIsEmpty");
+        
+        filledState.gameObject.SetActive(!isEmpty);
+        emptyState.gameObject.SetActive(isEmpty);
+    }
 }
