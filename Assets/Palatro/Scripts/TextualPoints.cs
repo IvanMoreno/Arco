cd0776b90631cfdfeb2eea7948bd1085b428f26b
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-08 12:29:55 +0200

    feat: animate points

--- Assets/Palatro/Scripts/TextualPoints.cs
+++ Assets/Palatro/Scripts/TextualPoints.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -6,18 +8,32 @@ namespace Palatro
 {
     public class TextualPoints : MonoBehaviour
     {
+        void Awake()
+        {
+            GetComponent<CanvasGroup>().alpha = 0;
+        }
+
         public async Task Show(int howMuch)
         {
-            GetComponent<TMP_Text>().text = $"Points: {howMuch}";
+            GetComponent<CanvasGroup>().alpha = 0;
+            GetComponentInChildren<TMP_Text>().text = $"Points: {howMuch}";
+            await GetComponent<CanvasGroup>().DOFade(1, 0.25f)
+                .SetEase(Ease.OutCubic)
+                .AsyncWaitForCompletion();
             await Task.Delay(1000);
-            GetComponent<TMP_Text>().text = string.Empty;
+
+            await GetComponent<CanvasGroup>().DOFade(0, 0.25f)
+                .SetEase(Ease.InCubic)
+                .AsyncWaitForCompletion();
+            GetComponentInChildren<TMP_Text>().text = string.Empty;
         }
 
         public async Task ShowError()
         {
-            GetComponent<TMP_Text>().text = "NOT VALID";
+            GetComponentInChildren<CanvasGroup>().alpha = 1;
+            GetComponentInChildren<TMP_Text>().text = "NOT VALID";
             await Task.Delay(1000);
-            GetComponent<TMP_Text>().text = string.Empty;
+            GetComponentInChildren<TMP_Text>().text = string.Empty;
         }
     }
 }
