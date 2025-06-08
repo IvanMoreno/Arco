58a004e13bc8099562e11e684e3e863aa91bd4bf
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-08 08:33:21 +0200

    refactor: sketch letter with points concept

---
+++ Assets/Palatro/Scripts/LetterWithPoints.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Palatro
+{
+    public class LetterWithPoints : MonoBehaviour
+    {
+        public string Letter { get; private set; }
+        public int Points { get; private set; }
+
+        public void Resemble(string letter, int points)
+        {
+            Assert.IsNotNull(letter);
+            Assert.IsTrue(points > 0);
+        
+            Letter = letter;
+            Points = points;
+
+            transform.Find("Character").GetComponent<TMP_Text>().text = Letter;
+            transform.Find("Points").GetComponent<TMP_Text>().text = Points.ToString();
+        }
+    }
+}
