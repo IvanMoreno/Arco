0f1e6b25a15bac4440272b0b11be4bcb4a7c009b
RGV <ricardo.guzman.velasco@outlook.com>
2025-07-12 08:08:40 +0200

    feat: next cycle

--- Assets/Stacklands/Moon.cs
+++ Assets/Stacklands/Moon.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,15 +8,36 @@ namespace Stacklands
 {
     public class Moon : MonoBehaviour
     {
-        void Start()
+        void Awake()
         {
             GetComponent<Image>().fillAmount = 0;
+        }
+        
+        async void Start()
+        {
+            await UntilFullMoon();
+            
+            var button = FindObjectsOfType<Button>(true).Single(x => x.name == "NextCycle");
+            button.gameObject.SetActive(true);
+            button.onClick.AddListener(() =>
+            {
+                GetComponent<Image>().fillAmount = 0;
+                button.gameObject.SetActive(false);
+            });
+        }
+
+        async Task UntilFullMoon()
+        {
+            while (GetComponent<Image>().fillAmount < 1f)
+                await Task.Yield();
         }
 
         void Update()
         {
-            GetComponent<Image>().fillAmount += Time.deltaTime / 60f;
-            
+            var cycleSeconds = 60f;
+            if (Input.GetKey(KeyCode.Space))
+                cycleSeconds /= 50;
+            GetComponent<Image>().fillAmount += Time.deltaTime / cycleSeconds;
         }
     }
 }
