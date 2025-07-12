b4a54be94f29e7db36e39714143bf24bcdbb00f3
RGV <ricardo.guzman.velasco@outlook.com>
2025-07-12 14:52:25 +0200

    fix: tweak radius spawning

--- Assets/Stacklands/BoosterPack.cs
+++ Assets/Stacklands/BoosterPack.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -6,7 +8,7 @@ namespace Stacklands
 {
     public class BoosterPack : MonoBehaviour
     {
-        const int spawnItemsRadius = 5;
+        const int spawnItemsRadius = 4;
         
         [SerializeField] GameObject[] cardsToSpawn;
 
@@ -26,17 +28,28 @@ namespace Stacklands
         async Task OpenPack()
         {
             var center = transform.position;
-            var angleIncrement = 2 * Mathf.PI / cardsToSpawn.Length;
-            for (var i = 0; i < cardsToSpawn.Length; i++)
+            var whereToSpawnEachCard = RadiusSpawning(cardsToSpawn.Length, center).ToList();
+
+            for (var i = 0; i < whereToSpawnEachCard.Count; i++)
             {
-                var angle = (i + 1) * angleIncrement;
-                var x = center.x + spawnItemsRadius * Mathf.Cos(angle);
-                var y = center.z + spawnItemsRadius * Mathf.Sin(angle);
-                FindAnyObjectByType<SpaceTime>().SpawnNearbyCard(cardsToSpawn[i], new Vector2(x, y));
+                FindAnyObjectByType<SpaceTime>().SpawnNearbyCard(cardsToSpawn[i], whereToSpawnEachCard[i]);
                 await Task.Delay(100);
             }
 
             Destroy(gameObject);
+        }
+
+        static IEnumerable<Vector2> RadiusSpawning(int howManyToSpawn, Vector2 center)
+        {
+            var centerTweak = center + new Vector2(-0.75f, 1.75f);
+            var angleIncrement = 2 * Mathf.PI / howManyToSpawn;
+            for (var i = 0; i < howManyToSpawn; i++)
+            {
+                var firstAngleTheTopOne = (i + 1) * angleIncrement;
+                var x = centerTweak.x + spawnItemsRadius * Mathf.Cos(firstAngleTheTopOne);
+                var y = centerTweak.y + spawnItemsRadius * Mathf.Sin(firstAngleTheTopOne);
+                yield return new Vector2(x, y);
+            }
         }
 
         void OnDrawGizmosSelected()
