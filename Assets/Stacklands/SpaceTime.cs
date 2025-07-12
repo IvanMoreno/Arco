d8a12d7902b3cd5d9b4f45b121d9c848e61cb7d5
RGV <ricardo.guzman.velasco@outlook.com>
2025-07-12 12:50:51 +0200

    chore: spacetime

---
+++ Assets/Stacklands/SpaceTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Stacklands
+{
+    public class SpaceTime : MonoBehaviour
+    {
+        //aquí van a ir las cosas también del tiempo que ahora mismo están en la luna. :)
+
+        public Stackable ClosestToBeStackedOn(Card card)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SpawnNearby(GameObject what, Vector2 whereabouts)
+        {
+            
+        }
+    }
+}
