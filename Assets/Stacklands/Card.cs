fc8038b9115b24899a97c1fee0be7d5fb422b974
RGV <ricardo.guzman.velasco@outlook.com>
2025-07-12 09:31:15 +0200

    chore: card concept

---
+++ Assets/Stacklands/Card.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Stacklands
+{
+    public class Card : MonoBehaviour
+    {
+        [SerializeField] string category;
+
+        public bool IsStackableOnMe(Card other)
+        {
+            if (category == "person")
+                return false;
+            return true;
+        }
+    }
+}
