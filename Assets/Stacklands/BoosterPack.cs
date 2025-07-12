10a6efe23a38f6ed7ef47f4dfd98c2a9855742d6
RGV <ricardo.guzman.velasco@outlook.com>
2025-07-12 09:57:12 +0200

    feat: spawn cards from booster

---
+++ Assets/Stacklands/BoosterPack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Stacklands
+{
+    public class BoosterPack : MonoBehaviour
+    {
+        [SerializeField] GameObject[] cardsToSpawn;
+
+        TaskCompletionSource<bool> untilClicked = new();
+        
+        async void Start()
+        {
+            await untilClicked.Task;
+            await OpenPack();
+        }
+        
+        void OnMouseDown()
+        {
+            untilClicked.TrySetResult(true);
+            Debug.Log("aksdfj");
+        }
+
+        async Task OpenPack()
+        {
+            for (var i = 0; i < cardsToSpawn.Length; i++)
+            {
+                var card = cardsToSpawn[i];
+                Instantiate(card, transform.position + Vector3.down * (i+1), Quaternion.identity);
+                
+                await Task.Delay(100);
+            }
+
+            Destroy(gameObject);
+
+        }
+    }
+}
