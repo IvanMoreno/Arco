5c46f4288dc6cc0b7f511d9d775dca3c57eab512
RGV <ricardo.guzman.velasco@outlook.com>
2025-07-12 13:49:24 +0200

    feat: stick from wood

--- Assets/Stacklands/Wood.cs
+++ Assets/Stacklands/Wood.cs
@@ -1,9 +1,59 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Stacklands
 {
     public class Wood : MonoBehaviour
     {
+        const float harvestDurationInSeconds = 2.75f;
         
+        [SerializeField] GameObject stickPrefab;
+        
+        float harvestProgressInSeconds;
+        int numberOfHarvests = 1;
+        
+        async void Start()
+        {
+            while (!destroyCancellationToken.IsCancellationRequested && numberOfHarvests > 0)
+                await HarvestLoop();
+
+            DestroyItself();
+        }
+
+        async Task HarvestLoop()
+        {
+            await Task.Yield();
+            if (!CanStartHarvest())
+            {
+                harvestProgressInSeconds = 0;
+                return;
+            }
+
+            harvestProgressInSeconds += Time.deltaTime;
+            if (harvestProgressInSeconds < harvestDurationInSeconds)
+                return;
+
+            SpawnStick();
+            harvestProgressInSeconds = 0;
+            numberOfHarvests--;
+        }
+
+        void SpawnStick()
+        {
+            FindAnyObjectByType<SpaceTime>().SpawnNearby(stickPrefab, transform.position);
+        }
+
+        bool CanStartHarvest()
+        {
+            return GetComponent<Stackable>().StackableOverMe.FirstOrDefault()?.TryGetComponent<Villager>(out _) ?? false;
+        }
+
+        void DestroyItself()
+        {
+            GetComponentInChildren<Villager>().GetComponent<Stackable>().RemoveFromStack();
+            Destroy(gameObject);
+        }
     }
 }
