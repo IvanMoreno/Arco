f05e42baf10fe7a8921647deb4a903c1c92ce81a
RGV <ricardo.guzman.velasco@outlook.com>
2025-07-12 08:01:28 +0200

    feat: moon cycle advances

---
+++ Assets/Stacklands/Moon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Stacklands
+{
+    public class Moon : MonoBehaviour
+    {
+        void Start()
+        {
+            GetComponent<Image>().fillAmount = 0;
+        }
+
+        void Update()
+        {
+            GetComponent<Image>().fillAmount += Time.deltaTime / 60f;
+            
+        }
+    }
+}
